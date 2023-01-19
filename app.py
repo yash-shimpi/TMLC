b4fecb8a80554d8a73e893b6f4bdefc652d43583
Yash Shimpi <yashmanish.shimpi.min20@itbhu.ac.in>
2023-01-19 14:49:29 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from gan import GAN
+
+st.set_page_config(page_icon="📊", page_title="CSV Wrangler", layout = "wide")
+
+st.markdown("<h1 style='text-align: center;'>Synthetic Data Generator App 🗂️</h1>", unsafe_allow_html=True)
+
+def main():
+    uploaded_file = st.file_uploader(
+            "",
+            key="1",
+            help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
+        )
+
+    if uploaded_file is not None:
+            file_container = st.expander("Check your uploaded .csv")
+            shows = pd.read_csv(uploaded_file)
+            uploaded_file.seek(0)
+            file_container.write(shows)
+
+    else:
+            st.info(
+                f"""
+                    👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
+                    """
+            )
+
+            st.stop()
+
+    df = pd.read_csv(
+        uploaded_file, 
+        na_values=['NA', '?'])
+
+    with st.form('Data Information'):
+        st.subheader("Enter following information for your data:")
+
+        Columns = st.text_input(label='Columns to be used (including Target column)')
+        Target = st.text_input(label='Target Column')
+
+        submit = st.form_submit_button(label='Submit')
+
+    if submit:
+        # print(Columns.dtype)
+        List_col = Columns.split(",")
+        List_tar = Target.split(",")
+        gen_data = GAN(df, List_col, List_tar)
+        # gen_data.to_csv('file.csv')
+        # st.download_button()
+
+        st.download_button(label = 'Download CSV', data = gen_data.to_csv(), file_name = 'gen.csv')
+            # st.write('Thanks for downloading!')
+
+if __name__ == '__main__':
+    main()
+
+#Data visualization metric
+#developed by .... 
+# Name,"Sex","Age","Height (in)","Weight (lbs)" 
---
+++ gan.py
@@ -0,0 +1,55 @@
+from tabgan.sampler import GANGenerator
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+
+def GAN(df, columns, target):
+    df = df[columns]
+
+    df = df.fillna(df.median(numeric_only=True).round(1))
+    
+    CATEGORICAL_COLS = list(set(df.columns) - set(df._get_numeric_data().columns))
+    le = preprocessing.LabelEncoder()
+    for i in CATEGORICAL_COLS:
+        df[i] = le.fit_transform(df[i].astype(str))
+    
+    # Split into training and test sets
+    df_x_train, df_x_test, df_y_train, df_y_test = train_test_split(
+        df.drop(target, axis=1),
+        df[target],
+        test_size=0.20,
+        #shuffle=False,
+        random_state=42,
+    )
+
+    # Create dataframe versions for tabular GAN
+    df_x_test, df_y_test = df_x_test.reset_index(drop=True), \
+    df_y_test.reset_index(drop=True)
+    df_y_train = pd.DataFrame(df_y_train)
+    df_y_test = pd.DataFrame(df_y_test)
+
+    gen_x, gen_y = GANGenerator(gen_x_times=1.1, cat_cols=None,
+            bot_filter_quantile=0.001, top_filter_quantile=0.999, \
+                is_post_process=True,
+            adversarial_model_params={
+                "metrics": "rmse", "max_depth": 2, "max_bin": 100, 
+                "learning_rate": 0.02, "random_state": \
+                    42, "n_estimators": 500,
+            }, pregeneration_frac=2, only_generated_data=False,\
+            gan_params = {"batch_size": 500, "patience": 25, \
+            "epochs" : 500,}).generate_data_pipe(df_x_train, df_y_train,\
+            df_x_test, deep_copy=True, only_adversarial=False, \
+            use_adversarial=True)
+
+    # gen_x = le.inverse_transform(gen_x)
+    targetdf = pd.DataFrame(gen_y)
+    targetdf.columns = target
+    # gen_x[target] = 
+    gen_x = pd.concat([gen_x, targetdf], axis=1)
+    # print(gen_x.shape)
+    print("HIIIII")
+    print(gen_x.shape)
+    return gen_x
+
+

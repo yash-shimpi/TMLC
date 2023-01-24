a3472a3bd2a9e43f698b595ee35891176338f357
Yash Shimpi <yashmanish.shimpi.min20@itbhu.ac.in>
2023-01-24 16:22:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from gan import GAN
 
 
 
-st.set_page_config(page_icon="📊", page_title="Data Generator", layout = "narrow")
+st.set_page_config(page_icon="📊", page_title="Data Generator", layout="centered")
 
 with open('style.css') as f:
      st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)

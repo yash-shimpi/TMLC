4f32c0b49c90a6e7335cd81dbf75e3c53ba12b7c
Yash Shimpi <yashmanish.shimpi.min20@itbhu.ac.in>
2023-01-24 16:16:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import numpy as np
 from gan import GAN
 
 
+
 st.set_page_config(page_icon="📊", page_title="CSV Wrangler", layout = "wide")
 
 with open('style.css') as f:
      st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
+     
 st.markdown("<h1 style='text-align: center;'>Synthetic Data Generator App 🗂️</h1>", unsafe_allow_html=True)
 
 def main():

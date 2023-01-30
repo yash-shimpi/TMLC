64d6afb94c148f0d23423bf3d0328600bbaf6189
Yash Shimpi <yashmanish.shimpi.min20@itbhu.ac.in>
2023-01-30 18:29:04 +0530

    Update Instructions.py

--- pages/Instructions.py
+++ pages/Instructions.py
@@ -7,8 +7,8 @@ import numpy as np
 
 st.set_page_config(page_icon="chart_with_upwards_trend", page_title="Data Generator", layout="centered")
 
-with open('style.css') as f:
-     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
+# with open('style.css') as f:
+#      st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 
 # st.markdown("<h1 style='text-align: center;'>Instructions</h1>", unsafe_allow_html=True)
 

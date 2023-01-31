4d68a375e19515c0ac4c90be5ec1d5c857d1a76b
Yash Shimpi <yashmanish.shimpi.min20@itbhu.ac.in>
2023-02-01 00:05:54 +0530

    k

--- Home.py
+++ Home.py
@@ -5,8 +5,8 @@ from gan import GAN
 
 st.set_page_config(page_icon="chart_with_upwards_trend", page_title="Data Generator", layout="centered")
 
-# with open('style.css') as f:
-#      st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
+with open('style.css') as f:
+     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 
 st.markdown("<h1 style='text-align: center;'>Synthetic Data Generator App 🗂️</h1>", unsafe_allow_html=True)
 

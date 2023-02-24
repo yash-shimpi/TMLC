8b54ad0b39e319d0e50f05cae98f2b29173cdb10
Yash Shimpi <yashmanish.shimpi.min20@itbhu.ac.in>
2023-02-24 20:33:28 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -75,59 +75,59 @@ def main():
         file_container_gen = st.expander("Check your Generated data")
         file_container_gen.write(gen_data)
 
-        # c1,c2=st.columns(2)
+        c1,c2=st.columns(2)
 
-        # c1.subheader("Original Data Summary")
-        # c1.write(df.describe())
+        c1.subheader("Original Data Summary")
+        c1.write(df.describe())
 
-        # c2.subheader("Generated Data Summary")
-        # c2.write(gen_data.describe())
+        c2.subheader("Generated Data Summary")
+        c2.write(gen_data.describe())
 
-        # # c3, c4 = st.columns(2)
+        # c3, c4 = st.columns(2)
 
-        # st.subheader("Correlation in original data: ")
-        # st.plotly_chart(corr_matrix(df))
+        st.subheader("Correlation in original data: ")
+        st.plotly_chart(corr_matrix(df))
 
-        # st.subheader("Correlation in generated data: ")
-        # st.plotly_chart(corr_matrix(gen_data))
+        st.subheader("Correlation in generated data: ")
+        st.plotly_chart(corr_matrix(gen_data))
 
         # Assume `df_original` is a Pandas dataframe of the original data
         # and `df_generated` is a Pandas dataframe of the generated data
-        for col in NUMERICAL_COLS:
-            if col == List_tar:
-                continue
+        # for col in NUMERICAL_COLS:
+        #     if col == List_tar:
+        #         continue
 
-            # Create a scatter plot using Plotly for the original data
-            fig_original = px.scatter(
-                x=df[col],
-                y=df[List_tar],
-                color_discrete_sequence=["blue"],
-                labels={
-                    "x": col,
-                    "y": List_tar,
-                },
-                title=f"{col} vs. {List_tar} Scatterplot (Original Data)",
-            )
+        #     # Create a scatter plot using Plotly for the original data
+        #     fig_original = px.scatter(
+        #         x=df[col],
+        #         y=df[List_tar],
+        #         color_discrete_sequence=["blue"],
+        #         labels={
+        #             "x": col,
+        #             "y": List_tar,
+        #         },
+        #         title=f"{col} vs. {List_tar} Scatterplot (Original Data)",
+        #     )
 
-            # Create a scatter plot using Plotly for the generated data
-            fig_generated = px.scatter(
-                x=gen_data[col],
-                y=gen_data[List_tar],
-                color_discrete_sequence=["red"],
-                labels={
-                    "x": col,
-                    "y": List_tar,
-                },
-                title=f"{col} vs. {List_tar} Scatterplot (Generated Data)",
-            )
+        #     # Create a scatter plot using Plotly for the generated data
+        #     fig_generated = px.scatter(
+        #         x=gen_data[col],
+        #         y=gen_data[List_tar],
+        #         color_discrete_sequence=["red"],
+        #         labels={
+        #             "x": col,
+        #             "y": List_tar,
+        #         },
+        #         title=f"{col} vs. {List_tar} Scatterplot (Generated Data)",
+        #     )
 
-            # Show the scatter plots side-by-side on Streamlit
-            st.write(f"### {col} vs. {List_tar}")
-            col1, col2 = st.beta_columns(2)
-            with col1:
-                st.plotly_chart(fig_original)
-            with col2:
-                st.plotly_chart(fig_generated)
+        #     # Show the scatter plots side-by-side on Streamlit
+        #     st.write(f"### {col} vs. {List_tar}")
+        #     col1, col2 = st.beta_columns(2)
+        #     with col1:
+        #         st.plotly_chart(fig_original)
+        #     with col2:
+        #         st.plotly_chart(fig_generated)
 
 
 if __name__ == '__main__':

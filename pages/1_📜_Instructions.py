e702c7a61a2bb51ed494e5511c74e8ff8f6b9bb8
Yash Shimpi <yashmanish.shimpi.min20@itbhu.ac.in>
2023-02-28 20:01:15 +0530

    Update 1_📜_Instructions.py

--- pages/1_📜_Instructions.py
+++ pages/1_📜_Instructions.py
@@ -28,3 +28,16 @@ st.write(
     1. Please mention atleast one target/label column 
     '''
 )
+
+st.title('Note:')
+
+st.write(
+    '''
+    1. This data generation technique uses GANs for generating data and each time new data is uploaded, GAN model is trained 
+    on the new data and the quality of generated data depends on this training.
+
+    2. Ideally for achieving best results, GAN model should be tuned each time but even without it, can achieve satisfactory results.
+
+    3. Generated data is statistically similar to the original data and not a replica or copy of it.
+    '''
+)

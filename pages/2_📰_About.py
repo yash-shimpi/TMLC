2bce1421d73492edbe527b3f7ed1840955dc8988
Yash Shimpi <yashmanish.shimpi.min20@itbhu.ac.in>
2023-03-06 18:07:59 +0530

    Update 2_📰_About.py

--- pages/2_📰_About.py
+++ pages/2_📰_About.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_icon="chart_with_upwards_trend", page_title="Data Generator", layout="centered")
+st.set_page_config(page_icon="chart_with_upwards_trend", page_title="Tableforge", layout="centered")
 
 st.title('About')
 
